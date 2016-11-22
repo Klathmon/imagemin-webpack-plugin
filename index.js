f1c9dcdfc75f9548280beaf9f43cfbf19ad0d1c8
Klathmon <gregbenner1@gmail.com>
2016-11-22 09:24:09 -0500

    add a compileTestOption function that allows `test` to be a glob or array of globs

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ import imageminJpegtran from 'imagemin-jpegtran'
 import imageminSvgo from 'imagemin-svgo'
 import { cpus } from 'os'
 import createThrottle from 'async-throttle'
+import { makeRe } from 'minimatch'
 
 export default class ImageminPlugin {
   constructor (options = {}) {
     // I love ES2015!
     const {
       disable = false,
-      test: testRegex = /.*/,
+      test = /.*/,
       optipng = {
         optimizationLevel: 3
       },
@@ -36,10 +37,8 @@ export default class ImageminPlugin {
       imageminOptions: {
         plugins: []
       },
-      testRegex
+      testRegex: compileTestOption(test)
     }
-
-    // TODO: Eventually allow globs and arrays to be passed as a test here and compile them to regex before moving on to the next part
 
     // As long as the options aren't `null` then include the plugin. Let the destructuring above
     // control whether the plugin is included by default or not.
@@ -74,7 +73,7 @@ export default class ImageminPlugin {
         await Promise.all(map(compilation.assets, (asset, filename) => throttle(async () => {
           // Skip the image if it's not a match for the regex
           if (testRegex.test(filename)) {
-            compilation.assets[filename] = await this.optimizeImage(asset, this.options.imageminOptions)
+            compilation.assets[filename] = await optimizeImage(asset, this.options.imageminOptions)
           }
         })))
 
@@ -85,24 +84,56 @@ export default class ImageminPlugin {
       }
     })
   }
+}
 
-  async optimizeImage (asset, imageminOptions) {
-    // Grab the orig source and size
-    const assetSource = asset.source()
-    const assetOrigSize = asset.size()
+/**
+ * Optimizes a single image asset, returning the orignal if the "optimized" version is larger
+ * @param  {Object}  asset
+ * @param  {Object}  imageminOptions
+ * @return {Promise(asset)}
+ */
+async function optimizeImage (asset, imageminOptions) {
+  // Grab the orig source and size
+  const assetSource = asset.source()
+  const assetOrigSize = asset.size()
 
-    // Ensure that the contents i have are in the form of a buffer
-    const assetContents = (Buffer.isBuffer(assetSource) ? assetSource : new Buffer(assetSource, 'utf8'))
+  // Ensure that the contents i have are in the form of a buffer
+  const assetContents = (Buffer.isBuffer(assetSource) ? assetSource : new Buffer(assetSource, 'utf8'))
 
-    // Await for imagemin to do the compression
-    const optimizedAssetContents = await imagemin.buffer(assetContents, imageminOptions)
+  // Await for imagemin to do the compression
+  const optimizedAssetContents = await imagemin.buffer(assetContents, imageminOptions)
 
-    // If the optimization actually produced a smaller file, then return the optimized version
-    if (optimizedAssetContents.length < assetOrigSize) {
-      return new RawSource(optimizedAssetContents)
-    } else {
-      // otherwize return the orignal
-      return asset
+  // If the optimization actually produced a smaller file, then return the optimized version
+  if (optimizedAssetContents.length < assetOrigSize) {
+    return new RawSource(optimizedAssetContents)
+  } else {
+    // otherwize return the orignal
+    return asset
+  }
+}
+
+/**
+ * Compiles a regex, glob, or an array of globs to a single regex for testing later
+ * @param  {RegExp|String|String[]} rawTestValue
+ * @return {RegExp}
+ */
+function compileTestOption (rawTestValue) {
+  const errorMessage = 'test parameter must be a regex, glob string, or an array of glob strings'
+  if (rawTestValue instanceof RegExp) {
+    // If it's a regex, just return it
+    return rawTestValue
+  } else if (typeof rawTestValue === 'string') {
+    // if it's a string, let minimatch convert it to a regex
+    return makeRe(rawTestValue)
+  } else if (Array.isArray(rawTestValue)) {
+    try {
+      // if it's an array of strings, then compile them all to a single regex and return that
+      return new RegExp(rawTestValue.map((test) => makeRe(test).source).join('|'))
+    } catch (err) {
+      throw new Error(errorMessage + ` "${err}"`)
     }
   }
+
+  // If we get here then something is wrong with what was passed into `test`, so throw since we can't deal with it
+  throw new Error(errorMessage)
 }
