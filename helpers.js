ce949a5e02e58baf71f46cff8fd0b1d402cadb1c
Gregory Benner <gregbenner1@gmail.com>
2017-12-12 14:33:37 -0500

    big clean up, splitting up the plugin into 2 files, and simplifying and rewriting a lot of the logic to a similar style across the whole plugin

---
+++ helpers.js
@@ -0,0 +1,152 @@
+import fs from 'fs'
+// import path from 'path'
+import path from 'path'
+import { makeRe } from 'minimatch'
+import imagemin from 'imagemin'
+import mkdirp from 'mkdirp'
+import promisify from 'util.promisify'
+
+export const readFile = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
+const mkdirpAsync = promisify(mkdirp)
+
+/**
+ * Optimizes a single image, returning the orignal if the "optimized" version is larger
+ * @param  {Object}  imageData
+ * @param  {Object}  imageminOptions
+ * @return {Promise(asset)}
+ */
+export async function optimizeImage (imageData, imageminOptions) {
+  // Ensure that the contents i have are in the form of a buffer
+  const imageBuffer = (Buffer.isBuffer(imageData) ? imageData : Buffer.from(imageData, 'utf8'))
+  // And get the original size for comparison later to make sure it actually got smaller
+  const originalSize = imageBuffer.length
+
+  // Await for imagemin to do the compression
+  const optimizedImageBuffer = await imagemin.buffer(imageBuffer, imageminOptions)
+
+  // If the optimization actually produced a smaller file, then return the optimized version
+  if (optimizedImageBuffer.length < originalSize) {
+    return optimizedImageBuffer
+  } else {
+    // otherwize return the orignal
+    return imageBuffer
+  }
+}
+
+/**
+ * Compiles a regex, glob, or an array of globs to an array of RegExp
+ * @param  {RegExp|RegExp[]|String|String[]} rawTestValue
+ * @return {RegExp[]}
+ */
+export function compileRegex (rawTestValue) {
+  const tests = Array.isArray(rawTestValue) ? rawTestValue : [rawTestValue]
+
+  return tests.map((test) => {
+    if (test instanceof RegExp) {
+      // If it's a regex, just return it
+      return test
+    } else if (typeof test === 'string') {
+      // If it's a string, let minimatch convert it to a regex
+      return makeRe(test)
+    } else {
+      throw new Error('test parameter must be a regex, glob string, or an array of regexes or glob strings')
+    }
+  })
+}
+
+/**
+ * Tests a filename to see if it matches any of the given test globs/regexes
+ * @param  {String} filename
+ * @param  {Array} regexes
+ * @return {Boolean}
+ */
+export function testFile (filename, regexes) {
+  for (let regex of regexes) {
+    if (regex.test(filename)) {
+      return true
+    }
+  }
+  return false
+}
+
+/**
+ * Tests if a file is within the given sizes
+ * @param  {assetSource} assetSource
+ * @param  {Number} minFileSize
+ * @param  {Number} maxFileSize
+ * @return {Boolean}
+ */
+export function testFileSize (assetSource, minFileSize, maxFileSize) {
+  return assetSource.length > minFileSize && assetSource.length <= maxFileSize
+}
+
+/**
+ * Invokes the passed in argument if it's a function
+ * @param  {Function|Any}  func
+ * @return {Any}
+ */
+export function invokeIfFunction (func) {
+  if (typeof func === 'function') {
+    return func()
+  } else {
+    return func
+  }
+}
+
+/**
+ * Gets the buffer of the file from cache. If it doesn't exist or the cache is
+ * not enabled, it will invoke elseFunc and use it's result as the result of the
+ * function.
+ * @param  {String} cacheFolder
+ * @param  {String} filename
+ * @param  {Function} elseFunc
+ * @return {Buffer}
+ */
+export async function getFromCacheIfPossible (cacheFolder, filename, elseFunc) {
+  const cacheFilePath = path.resolve(cacheFolder, filename)
+  if (cacheFolder !== null) {
+    if (await exists(cacheFilePath)) {
+      return readFile(cacheFilePath)
+    }
+  } else {
+    const fileBuffer = await elseFunc()
+    if (cacheFolder !== null) {
+      await writeFile(cacheFilePath, fileBuffer)
+    }
+    return fileBuffer
+  }
+}
+
+/**
+ * checks if a file/directory is accessable
+ * @param {any} directory
+ * @returns
+ */
+export async function exists (directory) {
+  return new Promise((resolve, reject) => {
+    fs.access(directory, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+      if (err) {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+
+/**
+ * async wrapper for writeFile that will create the directory if it does not already exist
+ * @param {String} filename
+ * @param {Buffer} buffer
+ * @returns
+ */
+export async function writeFile (filename, buffer) {
+  const directory = path.dirname(filename)
+  // if the directory doesn't exist, create it
+  if (!(await exists(directory))) {
+    await mkdirpAsync(directory)
+  }
+
+  return writeFileAsync(filename, buffer)
+}
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import fs from 'fs'
 import path from 'path'
 import { cpus } from 'os'
 import map from 'lodash.map'
-import imagemin from 'imagemin'
-import { makeRe } from 'minimatch'
 import imageminSvgo from 'imagemin-svgo'
 import createThrottle from 'async-throttle'
 import imageminOptipng from 'imagemin-optipng'
@@ -11,6 +8,16 @@ import imageminPngquant from 'imagemin-pngquant'
 import imageminGifsicle from 'imagemin-gifsicle'
 import imageminJpegtran from 'imagemin-jpegtran'
 import RawSource from 'webpack-sources/lib/RawSource'
+
+import {
+  compileRegex,
+  testFile,
+  testFileSize,
+  getFromCacheIfPossible,
+  readFile,
+  writeFile,
+  optimizeImage
+} from './helpers.js'
 
 export default class ImageminPlugin {
   constructor (options = {}) {
@@ -35,9 +42,9 @@ export default class ImageminPlugin {
       pngquant = null,
       externalImages = {
         sources: [],
-        destination: null
+        destination: '.'
       },
-      cacheFolder = ''
+      cacheFolder = null
     } = options
 
     this.options = {
@@ -48,7 +55,7 @@ export default class ImageminPlugin {
       imageminOptions: {
         plugins: []
       },
-      testRegexes: compileTestOption(test),
+      testRegexes: compileRegex(test),
       externalImages,
       cacheFolder
     }
@@ -77,238 +84,88 @@ export default class ImageminPlugin {
     // If disabled, short-circuit here and just return
     if (options.disable === true) return null
 
-    // Pull out options needed here
-    const {testRegexes, minFileSize, maxFileSize} = options
-    let {sources, destination} = options.externalImages
-
     // Access the assets once they have been assembled
     compiler.plugin('emit', async (compilation, callback) => {
+      // Create a throttle object which will limit the number of concurrent processes running
       const throttle = createThrottle(this.options.maxConcurrency)
-      if (typeof sources === 'function') {
-        sources = await sources()
-      }
-      if (typeof destination === 'function') {
-        destination = await destination()
-      }
 
       try {
-        await Promise.all([
-          optimizeWebpackImages(throttle, compilation, testRegexes, minFileSize, maxFileSize, options),
-          optimizeExternalImages(throttle, sources, destination, minFileSize, maxFileSize, options)
-        ])
+        // Optimise all images at the same time (throttled to maxConcurrency)
+        // and await until all of them to complete
+        const promises = this.optimizeWebpackImages(throttle, compilation).concat(this.optimizeExternalImages(throttle))
+        await Promise.all(promises)
+
         // At this point everything is done, so call the callback without anything in it
         callback()
       } catch (err) {
+        // if at any point we hit a snag, pass the error on to webpack
         callback(err)
       }
     })
   }
-}
 
-/**
- * Optimize images from webpack and put them back in the asset array when done
- * @param  {Function} throttle       The setup throttle library
- * @param  {Object} compilation      The compilation from webpack-sources
- * @param  {Regex} testRegexes       The regex to match if a specific image should be optimized
- * @param  {Integer} minFileSize     The minimum size of a file in bytes (files under this size will be skipped)
- * @param  {Integer} maxFileSize     The maximum size of a file in bytes (files over this size will be skipped)
- * @param  {Object} options          Plugin options
- * @return {Promise}                 Resolves when all images are done being optimized
- */
-async function optimizeWebpackImages (throttle, compilation, testRegexes, minFileSize, maxFileSize, options) {
-  const {cacheFolder} = options
+  /**
+   * Optimize images from webpack and put them back in the asset array when done
+   * @param  {Function} throttle       The setup throttle library
+   * @param  {Object} compilation      The compilation from webpack-sources
+   * @return {Promise[]}               An array of promises that resolve when each image is done being optimized
+   */
+  optimizeWebpackImages (throttle, compilation) {
+    const {
+      testRegexes,
+      minFileSize,
+      maxFileSize,
+      cacheFolder
+    } = this.options
 
-  return Promise.all(map(compilation.assets, (asset, filename) => throttle(async () => {
-    const assetSource = asset.source()
-    // Skip the image if it's not a match for the regex
-    if (testFile(filename, testRegexes) && testFileSize(assetSource, minFileSize, maxFileSize)) {
-      // Optimize the asset's source
+    // Return an array of promises that resolve when each file is done being optimized
+    // pass everything through the throttle function to limit maximum concurrency
+    return map(compilation.assets, (asset, filename) => throttle(async () => {
+      const assetSource = asset.source()
+      // Skip the image if it's not a match for the regex or it's too big/small
+      if (testFile(filename, testRegexes) && testFileSize(assetSource, minFileSize, maxFileSize)) {
+        // Use the helper function to get the file from cache if possible, or
+        // run the optimize function and store it in the cache when done
+        let optimizedImageBuffer = getFromCacheIfPossible(cacheFolder, filename, () => {
+          return optimizeImage(assetSource, this.options.imageminOptions)
+        })
 
-      if (cacheFolder) {
-        try {
-          const cachedOptimizedImageBuffer = await readFile(`${cacheFolder}/${filename}`)
-          compilation.assets[filename] = new RawSource(cachedOptimizedImageBuffer)
-
-          return
-        } catch (e) {
-          // cached file doesn't exist yet
-        }
+        // Then write the optimized version back to the asset object as a "raw source"
+        compilation.assets[filename] = new RawSource(optimizedImageBuffer)
       }
-
-      const optimizedImageBuffer = await optimizeImage(assetSource, options.imageminOptions)
-
-      if (cacheFolder) {
-        await writeFile(`${cacheFolder}/${filename}`, optimizedImageBuffer)
-      }
-
-      // Then write the optimized version back to the asset object as a "raw source"
-      compilation.assets[filename] = new RawSource(optimizedImageBuffer)
-    }
-  })))
-}
-
-/**
- * Optimizes external images,
- * @param  {[type]} throttle        [description]
- * @param  {[type]} sources         [description]
- * @param  {[type]} destination     [description]
- * @param  {[type]} minFileSize     [description]
- * @param  {[type]} maxFileSize     [description]
- * @param  {[type]} options         [description]
- * @return {[type]}                 [description]
- */
-async function optimizeExternalImages (throttle, sources, destination, minFileSize, maxFileSize, options) {
-  const {cacheFolder} = options
-
-  return Promise.all(map(sources, (filename) => throttle(async () => {
-    const fileData = await readFile(filename)
-    if (testFileSize(fileData, minFileSize, maxFileSize)) {
-      // If the destination is provided use it, otherwise overwrite the image in place
-      if (typeof destination !== 'string') {
-        destination = '.'
-      }
-
-      const writeFilePath = path.normalize(`${destination}/${filename}`)
-
-      if (cacheFolder) {
-        try {
-          const cachedOptimizedImageBuffer = await readFile(`${cacheFolder}/${filename}`)
-          return writeFile(writeFilePath, cachedOptimizedImageBuffer)
-        } catch (e) {
-          // cached file doesn't exist yet
-        }
-      }
-
-      // Read in the file and optimize it
-      const optimizedImageBuffer = await optimizeImage(await readFile(filename), options.imageminOptions)
-
-      if (cacheFolder) {
-        await writeFile(`${cacheFolder}/${filename}`, optimizedImageBuffer)
-      }
-
-      return writeFile(writeFilePath, optimizedImageBuffer)
-    }
-  })))
-}
-
-/**
- * Optimizes a single image, returning the orignal if the "optimized" version is larger
- * @param  {Object}  imageData
- * @param  {Object}  imageminOptions
- * @return {Promise(asset)}
- */
-async function optimizeImage (imageData, imageminOptions) {
-  // Ensure that the contents i have are in the form of a buffer
-  const imageBuffer = (Buffer.isBuffer(imageData) ? imageData : Buffer.from(imageData, 'utf8'))
-  // And get the original size for comparison later to make sure it actually got smaller
-  const originalSize = imageBuffer.length
-
-  // Await for imagemin to do the compression
-  const optimizedImageBuffer = await imagemin.buffer(imageBuffer, imageminOptions)
-
-  // If the optimization actually produced a smaller file, then return the optimized version
-  if (optimizedImageBuffer.length < originalSize) {
-    return optimizedImageBuffer
-  } else {
-    // otherwize return the orignal
-    return imageBuffer
+    }))
   }
-}
 
-/**
- * Tests a filename to see if it matches any of the given test globs/regexes
- * @param  {String} filename
- * @param  {Array} regexes
- * @return {Boolean}
- */
-function testFile (filename, regexes) {
-  for (let regex of regexes) {
-    if (regex.test(filename)) {
-      return true
-    }
+  /**
+   * Optimizes external images
+   * @param  {Function} throttle The setup throttle library
+   * @return {Promise[]}         An array of promises that resolve when each image is done being optimized
+   */
+  optimizeExternalImages (throttle) {
+    const {
+      externalImages: {
+        sources,
+        destination
+      },
+      minFileSize,
+      maxFileSize,
+      cacheFolder
+    } = this.options
+
+    return map(sources, (filename) => throttle(async () => {
+      const fileData = await readFile(filename)
+      if (testFileSize(fileData, minFileSize, maxFileSize)) {
+        const writeFilePath = path.resolve(destination, filename)
+
+        // Use the helper function to get the file from cache if possible, or
+        // run the optimize function and store it in the cache when done
+        let optimizedImageBuffer = getFromCacheIfPossible(cacheFolder, filename, async () => {
+          return optimizeImage(await readFile(filename), this.options.imageminOptions)
+        })
+
+        // Write the file to the destination when done
+        return writeFile(writeFilePath, optimizedImageBuffer)
+      }
+    }))
   }
-  return false
-}
-
-function testFileSize (assetSource, minFileSize, maxFileSize) {
-  return assetSource.length > minFileSize && assetSource.length <= maxFileSize
-}
-
-/**
- * Compiles a regex, glob, or an array of globs to a single regex for testing later
- * @param  {RegExp|String|String[]} rawTestValue
- * @return {RegExp}
- */
-function compileTestOption (rawTestValue) {
-  const tests = Array.isArray(rawTestValue) ? rawTestValue : [rawTestValue]
-
-  return tests.map((test) => {
-    if (test instanceof RegExp) {
-      // If it's a regex, just return it
-      return test
-    } else if (typeof test === 'string') {
-      // If it's a string, let minimatch convert it to a regex
-      return makeRe(test)
-    } else {
-      throw new Error('test parameter must be a regex, glob string, or an array of regexes or glob strings')
-    }
-  })
-}
-
-/**
- * async wrapper for fs readFile.
- * @param {any} filename
- * @returns * @return {Promise(buffer)}
- */
-async function readFile (filename) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, '', (error, result) => {
-      if (error) {
-        return reject(error)
-      }
-      resolve(result)
-    })
-  })
-}
-
-/**
- * async wrapper for exists
- * @param {any} directory
- * @returns
- */
-async function exists (directory) {
-  return new Promise((resolve, reject) => {
-    fs.access(directory, fs.constants.R_OK | fs.constants.W_OK, (err) => {
-      if (err) resolve(false)
-
-      resolve(true)
-    })
-  })
-}
-
-/**
- * async wrapper for writeFile
- * @param {any} filename
- * @param {any} buffer
- * @returns
- */
-async function writeFile (filename, buffer) {
-  return new Promise((resolve, reject) => {
-    const doWrite = () => {
-      fs.writeFile(filename, buffer, (error) => {
-        if (error) {
-          return reject(error)
-        }
-        resolve()
-      })
-    }
-    const directory = path.dirname(filename)
-    exists(directory).then((exists) => {
-      if (!exists) {
-        fs.mkdir(directory, doWrite)
-      } else {
-        doWrite()
-      }
-    })
-  })
 }
